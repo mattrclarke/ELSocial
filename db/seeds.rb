96b7f2e0d4014cc08423065fd4e3578248401275
Matt Clarke <matt.clarke85@gmail.com>
2019-09-20 19:11:57 +1000

    update seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,6 +2,8 @@ user = User.create(email: "a@a.com", password: 'a')
 profile = Profile.create(user_id: user.id, first_name: "Matt", last_name: "Clarke")
 feed = Feed.create(profile_id: profile.id, user_id: user.id)
 
-user = User.create(email: "x@x.com", password: 'x')
-profile = Profile.create(user_id: user.id, first_name: "Tim", last_name: "Thompson")
-feed = Feed.create(profile_id: profile.id, user_id: user.id)
+10.times do
+  user = User.create(email: Faker::Internet.email, password: 'password')
+  profile = Profile.create(user_id: user.id, first_name: Faker::Name.first_name, last_name: Faker::Name.last_name)
+  feed = Feed.create(profile_id: profile.id, user_id: user.id)
+end
